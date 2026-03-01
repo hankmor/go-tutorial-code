ee4b6b8ea006cf6b3349f97cb5ff0a96230e42cc
hankmo <hankmo.x@gmail.com>
2026-03-01 14:00:45 +0800

    feat: new hello, program structure sample codes

---
+++ 01-basic/program_structure.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Go 程序结构说明")
+    fmt.Println("================")
+    fmt.Println("1. package main - 定义包名")
+    fmt.Println("2. import \"fmt\" - 导入包")
+    fmt.Println("3. func main() - 主函数入口")
+    fmt.Println("4. fmt.Println - 调用函数")
+}
